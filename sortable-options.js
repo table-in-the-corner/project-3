d1e89cc85a6b2be314ab03bb164109e77d7de592
Bailey Pardee <baileypardee@gmail.com>
2021-11-11 14:22:40 -0500

    options

---
+++ sortable-options.js
@@ -0,0 +1,5 @@
+// import { Sortable } from './src/Sortable.js';
+import { SortableOption } from './src/SortableOption.js';
+
+// customElements.define(Sortable.tag, Sortable);
+customElements.define(SortableOption.tag, SortableOption);
